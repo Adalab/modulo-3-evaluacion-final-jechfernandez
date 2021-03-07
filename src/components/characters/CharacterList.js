175eb8b98b48db637dbdeeba605fc75670af78d8
jechfernandez <jechfernandez@gmail.com>
2021-03-07 11:48:38 +0100

    Add results for when the character does not exist

--- src/components/characters/CharacterList.js
+++ src/components/characters/CharacterList.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import '../../stylesheets/characters/CharacterList.scss';
 import Character from './Character';
+import {Link} from 'react-router-dom';
+import whoops from '../../images/pickle.png'
 
-const CharacterList = props => {
-  const characterElements = props.characters.map (character => {
-    return (
-      <li className="character__list" key={character.id}>
-        <Character character={character} />
-      </li>
-    );
-  });
-  return (
-    <article className="character__article">
-      <ul className="list">
-        {characterElements}
-      </ul>
-    </article>
-  );
-};
+function CharacterList(props) {
+  const characterElements = props.characters.map(character => {
+      return <li key={character.id} className="character__list"> <Character character={character} /> </li>
+  })
+  if (characterElements.length === 0) {
+      return (
+        <div className="detail__error">
+        <header className="detail__error--link">
+          <Link to="/">
+            <span className="detail__error--notfound">Take me back home</span>
+          </Link>
+        </header>
 
+        <div className="detail__error--img">
+          <img
+            className="detail__error--img-src"
+            src={whoops}
+            alt="Error, not found"
+          />
+        </div>
+      </div>
+      )
+  } else {
+
+      return (
+        <article className="character__article">
+               <ul className="list">
+                 {characterElements}
+               </ul>
+             </article>
+      );
+  }
+}
 export default CharacterList;
+
