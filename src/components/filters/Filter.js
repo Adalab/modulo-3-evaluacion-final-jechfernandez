44a1154f16ce8c51e98845eb15f5128d4c002319
jechfernandez <jechfernandez@gmail.com>
2021-03-09 16:16:58 +0100

    Begin adding new filters

--- src/components/filters/Filter.js
+++ src/components/filters/Filter.js
@@ -5,6 +5,7 @@ import FilterByName from './FilterByName';
 import FilterByGender from './FilterByGender';
 import FilterBySpecies from './FilterBySpecies';
 import FilterByStatus from './FilterByStatus';
+import FilterByOrigin from './FilterByOrigin';
 import ResetButton from '../ResetButton';
 
 const Filter = props => {
@@ -19,6 +20,7 @@ const Filter = props => {
         <FilterByGender handleFilter={props.handleFilter} gender={props.gender}/>
         <FilterBySpecies handleFilter={props.handleFilter} species={props.species}/>
         <FilterByStatus handleFilter={props.handleFilter} status={props.status} />
+        <FilterByOrigin handleFilter={props.handleFilter} status={props.status}/>
       </form>
       <ResetButton handleReset={props.handleReset} />
     </section>
